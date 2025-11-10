8d4377fee2f4410073e3aa1fc8dd1790c48270e1
aflor45123 <aflor45@uic.edu>
2025-11-10 11:01:32 -0600

    Added template file

---
+++ Chess-Clocks-Project.ino
@@ -0,0 +1,54 @@
+/*-----------------------------------------------
+Name 1: Andres Flores / aflor45 / aflor45@uic.edu
+Code contribution: Primary Arduino code
+
+Name 2: Paola Magana  / pmaga2 / pmaga2@uic.edu
+Code contribution: Secondary Arduino code
+
+Name 3: Katie Lee-Olmstead / kleeo2@uic.edu
+Code contribution: Communication between controller and primary
+
+Name 4: Jennifer Khamdaraphone / jkham2 / jkham2@uic.edu
+Code contribution: Communication between primary and secondary
+
+Group number: 10
+
+Project: The Final Countdown
+Description: With this project, we are creating our own version of a chess clock. 
+This chess clock will have multiple features such as: player name display, digital 
+and analog time display, fan, and LEDs.
+
+References:
+- https://docs.arduino.cc/built-in-examples/digital/BlinkWithoutDelay/ 
+- https://docs.arduino.cc/built-in-examples/digital/Button/
+- https://docs.arduino.cc/built-in-examples/digital/Debounce/
+- https://docs.arduino.cc/learn/electronics/lcd-displays/
+- https://docs.arduino.cc/language-reference/en/functions/advanced-io/tone/
+- https://docs.arduino.cc/libraries/servo/
+- https://docs.arduino.cc/language-reference/en/functions/communication/serial/
+- https://projecthub.arduino.cc/herolivechannel/chess-alarm-clock-using-arduino-rotary-encoder-7-segment-1511f2
+- https://docs.arduino.cc/learn/communication/wire/
+- https://www.instructables.com/I2C-between-Arduinos/
+- https://github.com/fmalpartida/New-LiquidCrystal
+----------------------------------------------*/
+#include <LiquidCrystal.h>
+
+void setup() {
+
+}
+
+
+void loop() {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
